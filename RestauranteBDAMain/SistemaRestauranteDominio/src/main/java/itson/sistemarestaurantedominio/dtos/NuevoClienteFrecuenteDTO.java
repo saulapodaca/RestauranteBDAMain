bd3e715dfe165853081e632fb013f9786db8f62a
RaulNav <ari.montoya252825@potros.itson.edu.mx>
2025-04-02 01:35:40 -0700

    Nueva DTO: NuevoClienteFrecuente
    DTO para NuevoClienteFrecuente

---
+++ RestauranteBDAMain/SistemaRestauranteDominio/src/main/java/itson/sistemarestaurantedominio/dtos/NuevoClienteFrecuenteDTO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package itson.sistemarestaurantedominio.dtos;
+
+/**
+ *
+ * @author rauln
+ */
+public class NuevoClienteFrecuenteDTO {
+    
+}
