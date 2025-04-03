e45570deb3b91ca9891a1a7dad7b32990d5bc210
RaulNav <ari.montoya252825@potros.itson.edu.mx>
2025-04-03 13:33:34 -0700

    Revert "Cambios de diseño en la clase RegistrarProductos"
    This reverts commit 39872f6ce1d978cde60564e7947c4475eb358484.

--- RestauranteBDAMain/SistemaRestaurantePresentacion/src/main/java/itson/sistemarestaurantepresentacion/RegistrarProductos.java
+++ RestauranteBDAMain/SistemaRestaurantePresentacion/src/main/java/itson/sistemarestaurantepresentacion/RegistrarProductos.java
@@ -32,159 +32,115 @@ public class RegistrarProductos extends javax.swing.JFrame {
 
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextPane1 = new javax.swing.JTextPane();
-        background = new javax.swing.JPanel();
-        labelUnidadMedida = new javax.swing.JLabel();
-        registrarNuevoIngredienteTxt2 = new javax.swing.JLabel();
-        labelNombre = new javax.swing.JLabel();
-        labelStock = new javax.swing.JLabel();
-        jSeparator1 = new javax.swing.JSeparator();
-        jSeparator2 = new javax.swing.JSeparator();
-        jSeparator6 = new javax.swing.JSeparator();
-        botonRegistrar = new javax.swing.JLabel();
-        textFieldIngresarStock = new javax.swing.JTextField();
-        comboBoxUnidadMedida = new javax.swing.JComboBox<>();
-        textFieldIngresarNombre = new javax.swing.JTextField();
-        labelUnidadMedida1 = new javax.swing.JLabel();
-        textFieldIngresarNombre1 = new javax.swing.JTextField();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        botonRegistarProducto = new javax.swing.JLabel();
 
         jScrollPane2.setViewportView(jTextPane1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        background.setBackground(new java.awt.Color(255, 255, 255));
-        background.setMinimumSize(new java.awt.Dimension(300, 400));
-        background.setPreferredSize(new java.awt.Dimension(300, 400));
-        background.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3"
+            }
+        ));
+        jScrollPane3.setViewportView(jTable2);
 
-        labelUnidadMedida.setFont(new java.awt.Font("Montserrat SemiBold", 1, 12)); // NOI18N
-        labelUnidadMedida.setText("PRECIO *Obligatorio*");
-        background.add(labelUnidadMedida, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 110, -1, -1));
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
 
-        registrarNuevoIngredienteTxt2.setFont(new java.awt.Font("Montserrat SemiBold", 1, 12)); // NOI18N
-        registrarNuevoIngredienteTxt2.setText("REGISTRAR PRODUCTO");
-        background.add(registrarNuevoIngredienteTxt2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 20, -1, -1));
+        jLabel1.setText("Nombre");
 
-        labelNombre.setFont(new java.awt.Font("Montserrat SemiBold", 1, 12)); // NOI18N
-        labelNombre.setText("NOMBRE *Obligatorio*");
-        background.add(labelNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 60, -1, -1));
+        jLabel2.setText("Precio");
 
-        labelStock.setFont(new java.awt.Font("Montserrat SemiBold", 1, 12)); // NOI18N
-        labelStock.setText("BUSCAR INGREDIENTES *1 Por Lo Menos*");
-        background.add(labelStock, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 160, -1, -1));
-        background.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 105, 240, 5));
-        background.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 155, 240, 5));
-        background.add(jSeparator6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 305, 240, 5));
+        jLabel3.setText("Tipo de Producto");
 
-        botonRegistrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/REGISTRAR.png"))); // NOI18N
-        botonRegistrar.setToolTipText("");
-        botonRegistrar.addMouseListener(new java.awt.event.MouseAdapter() {
+        botonRegistarProducto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/REGISTRAR.png"))); // NOI18N
+        botonRegistarProducto.setToolTipText("");
+        botonRegistarProducto.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                botonRegistrarMouseClicked(evt);
+                botonRegistarProductoMouseClicked(evt);
             }
         });
-        background.add(botonRegistrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 220, 100, 40));
-
-        textFieldIngresarStock.setFont(new java.awt.Font("Montserrat Medium", 0, 12)); // NOI18N
-        textFieldIngresarStock.setForeground(new java.awt.Color(204, 204, 204));
-        textFieldIngresarStock.setText("INGRESE EL STOCK INICIAL");
-        textFieldIngresarStock.setBorder(null);
-        background.add(textFieldIngresarStock, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 180, 250, -1));
-
-        comboBoxUnidadMedida.setFont(new java.awt.Font("Montserrat Medium", 0, 12)); // NOI18N
-        comboBoxUnidadMedida.setForeground(new java.awt.Color(204, 204, 204));
-        comboBoxUnidadMedida.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "PIEZAS", "GRAMOS", "MILILITROS" }));
-        comboBoxUnidadMedida.setBorder(null);
-        comboBoxUnidadMedida.addItemListener(new java.awt.event.ItemListener() {
-            public void itemStateChanged(java.awt.event.ItemEvent evt) {
-                comboBoxUnidadMedidaItemStateChanged(evt);
-            }
-        });
-        comboBoxUnidadMedida.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                comboBoxUnidadMedidaActionPerformed(evt);
-            }
-        });
-        background.add(comboBoxUnidadMedida, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 130, 80, 20));
-        comboBoxUnidadMedida.setSelectedIndex(1);
-
-        textFieldIngresarNombre.setFont(new java.awt.Font("Montserrat Medium", 0, 12)); // NOI18N
-        textFieldIngresarNombre.setForeground(new java.awt.Color(204, 204, 204));
-        textFieldIngresarNombre.setText("INGRESE EL PRECIO");
-        textFieldIngresarNombre.setBorder(null);
-        textFieldIngresarNombre.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                textFieldIngresarNombreActionPerformed(evt);
-            }
-        });
-        background.add(textFieldIngresarNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 130, 130, -1));
-
-        labelUnidadMedida1.setFont(new java.awt.Font("Montserrat SemiBold", 1, 12)); // NOI18N
-        labelUnidadMedida1.setText("Tipo *Obligatorio*");
-        background.add(labelUnidadMedida1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 110, -1, -1));
-
-        textFieldIngresarNombre1.setFont(new java.awt.Font("Montserrat Medium", 0, 12)); // NOI18N
-        textFieldIngresarNombre1.setForeground(new java.awt.Color(204, 204, 204));
-        textFieldIngresarNombre1.setText("INGRESE EL NOMBRE DEL PRODUCTO");
-        textFieldIngresarNombre1.setBorder(null);
-        textFieldIngresarNombre1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                textFieldIngresarNombre1ActionPerformed(evt);
-            }
-        });
-        background.add(textFieldIngresarNombre1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 80, 250, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(89, 89, 89)
+                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(91, 91, 91)
+                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(124, 124, 124)
+                        .addComponent(jLabel3)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(91, 91, 91)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 531, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(126, 126, 126)
+                        .addComponent(jLabel1)))
+                .addContainerGap(117, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(126, Short.MAX_VALUE)
-                .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(62, 62, 62))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(botonRegistarProducto)
+                .addGap(308, 308, 308))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(background, javax.swing.GroupLayout.PREFERRED_SIZE, 326, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(131, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(42, 42, 42)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                .addComponent(botonRegistarProducto)
+                .addGap(17, 17, 17))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void botonRegistrarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonRegistrarMouseClicked
-        try{
-            String nombre = textFieldIngresarNombre.getText().trim();
-            float stock = Float.parseFloat(textFieldIngresarStock.getText());
-            String unidadSeleccionada = comboBoxUnidadMedida.getSelectedItem().toString();
-            UnidadMedidaIngrediente unidadMedida = UnidadMedidaIngrediente.valueOf(unidadSeleccionada);
-
-            NuevoIngredienteDTO ingredienteDTO = new NuevoIngredienteDTO(nombre, unidadMedida, stock);
-            this.ingredientesBO.registrar(ingredienteDTO);
-
-            JOptionPane.showMessageDialog(background, "Registro de ingrediente exitoso!", "Éxito", JOptionPane.INFORMATION_MESSAGE);
-            limpiarFormulario();
-        }catch(IngredienteRegistradoException | NombreInvalidoException | StockInvalidoException e){
-            JOptionPane.showMessageDialog(background, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-        }
-    }//GEN-LAST:event_botonRegistrarMouseClicked
-
-    private void comboBoxUnidadMedidaItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_comboBoxUnidadMedidaItemStateChanged
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_comboBoxUnidadMedidaItemStateChanged
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
-    private void comboBoxUnidadMedidaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboBoxUnidadMedidaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_comboBoxUnidadMedidaActionPerformed
-
-    private void textFieldIngresarNombreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textFieldIngresarNombreActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_textFieldIngresarNombreActionPerformed
-
-    private void textFieldIngresarNombre1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textFieldIngresarNombre1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_textFieldIngresarNombre1ActionPerformed
+    private void botonRegistarProductoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonRegistarProductoMouseClicked
+       
+    }//GEN-LAST:event_botonRegistarProductoMouseClicked
 
     /**
      * @param args the command line arguments
@@ -222,21 +178,16 @@ public class RegistrarProductos extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel background;
-    private javax.swing.JLabel botonRegistrar;
-    private javax.swing.JComboBox<String> comboBoxUnidadMedida;
+    private javax.swing.JLabel botonRegistarProducto;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JSeparator jSeparator2;
-    private javax.swing.JSeparator jSeparator6;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
     private javax.swing.JTextPane jTextPane1;
-    private javax.swing.JLabel labelNombre;
-    private javax.swing.JLabel labelStock;
-    private javax.swing.JLabel labelUnidadMedida;
-    private javax.swing.JLabel labelUnidadMedida1;
-    private javax.swing.JLabel registrarNuevoIngredienteTxt2;
-    private javax.swing.JTextField textFieldIngresarNombre;
-    private javax.swing.JTextField textFieldIngresarNombre1;
-    private javax.swing.JTextField textFieldIngresarStock;
     // End of variables declaration//GEN-END:variables
 }
