ad8708b19069cbb0536c4b348502eed412c3cdba
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-04-03 23:13:31 -0700

    Actualizacion comanda dominio

--- RestauranteBDAMain/SistemaRestauranteDominio/src/main/java/itson/sistemarestaurantedominio/Comanda.java
+++ RestauranteBDAMain/SistemaRestauranteDominio/src/main/java/itson/sistemarestaurantedominio/Comanda.java
@@ -3,12 +3,22 @@ package itson.sistemarestaurantedominio;
 //@author SAUL ISAAC APODACA BALDENEGRO 00000252020
 
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 
 @Entity
@@ -22,6 +32,92 @@ public class Comanda implements Serializable {
 
     @Column(name = "folio", nullable = false, unique = true, length = 15)
     private String folio;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fechaCreacion", nullable = false)
+    private Calendar fechaCreacion;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(name = "estadoComanda", nullable = false)
+    private EstadoComanda estadoComanda;
+    
+    @Column(name = "totalAcumulado", nullable = false)
+    private float totalAcumulado;
+    
+    @ManyToOne
+    @JoinColumn(name = "idClienteFrecuente", nullable = true)
+    private ClienteFrecuente clienteFrecuente;
+    
+    @OneToMany(mappedBy = "comanda", cascade = CascadeType.ALL)
+    private List<DetalleComanda> detalles;
+
+    public Comanda() {
+    }
+    
+    public Comanda(String folio, Calendar fechaCreacion, EstadoComanda estadoComanda, float totalAcumulado, ClienteFrecuente clienteFrecuente, List<DetalleComanda> detalles) {
+        this.folio = folio;
+        this.fechaCreacion = fechaCreacion;
+        this.estadoComanda = estadoComanda;
+        this.totalAcumulado = totalAcumulado;
+        this.clienteFrecuente = clienteFrecuente;
+        this.detalles = detalles;
+    }
+    
+    public Comanda(String folio, Calendar fechaCreacion, EstadoComanda estadoComanda, float totalAcumulado, List<DetalleComanda> detalles) {
+        this.folio = folio;
+        this.fechaCreacion = fechaCreacion;
+        this.estadoComanda = estadoComanda;
+        this.totalAcumulado = totalAcumulado;
+        this.detalles = detalles;
+    }    
+    
+    public String getFolio() {
+        return folio;
+    }
+
+    public void setFolio(String folio) {
+        this.folio = folio;
+    }
+
+    public Calendar getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Calendar fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public EstadoComanda getEstadoComanda() {
+        return estadoComanda;
+    }
+
+    public void setEstadoComanda(EstadoComanda estadoComanda) {
+        this.estadoComanda = estadoComanda;
+    }
+
+    public float getTotalAcumulado() {
+        return totalAcumulado;
+    }
+
+    public void setTotalAcumulado(float totalAcumulado) {
+        this.totalAcumulado = totalAcumulado;
+    }
+
+    public ClienteFrecuente getClienteFrecuente() {
+        return clienteFrecuente;
+    }
+
+    public void setClienteFrecuente(ClienteFrecuente clienteFrecuente) {
+        this.clienteFrecuente = clienteFrecuente;
+    }
+
+    public List<DetalleComanda> getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(List<DetalleComanda> detalles) {
+        this.detalles = detalles;
+    } 
     
     public Long getId() {
         return id;
