e6bb9cb074d98a3c31cee519fa387f77552a6a09
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-04-03 23:13:43 -0700

    actualizacion detallecomanda dominio

--- RestauranteBDAMain/SistemaRestauranteDominio/src/main/java/itson/sistemarestaurantedominio/DetalleComanda.java
+++ RestauranteBDAMain/SistemaRestauranteDominio/src/main/java/itson/sistemarestaurantedominio/DetalleComanda.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 
@@ -20,6 +22,89 @@ public class DetalleComanda implements Serializable {
     @Column(name = "idDetallesComandas")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "folio")
+    private Comanda comanda;
+    
+    @ManyToOne
+    @JoinColumn(name = "idProducto")
+    private Producto producto;
+    
+    @Column(name = "cantidad", nullable = false)
+    private int cantidad;
+    
+    @Column(name = "precioUnitario", nullable = false)
+    private float precioUnitario;
+    
+    @Column(name = "importeTotal", nullable = false)
+    private float importeTotal;
+    
+    @Column(name = "nota", nullable = true, length = 255)
+    private String nota;
+
+    public DetalleComanda() {
+    }
+
+    public DetalleComanda(Comanda comanda, Producto producto, int cantidad, float precioUnitario, float importeTotal, String nota) {
+        this.comanda = comanda;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.importeTotal = importeTotal;
+        this.nota = nota;
+    }
+
+    
+    
+    public Comanda getComanda() {
+        return comanda;
+    }
+
+    public void setComanda(Comanda comanda) {
+        this.comanda = comanda;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public float getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(float precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public float getImporteTotal() {
+        return importeTotal;
+    }
+
+    public void setImporteTotal(float importeTotal) {
+        this.importeTotal = importeTotal;
+    }
+
+    public String getNota() {
+        return nota;
+    }
+
+    public void setNota(String nota) {
+        this.nota = nota;
+    }
+    
+    
     
     public Long getId() {
         return id;
