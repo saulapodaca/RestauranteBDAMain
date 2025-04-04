8b25516b5207b149b66a9272ba509f957ec13f72
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-04-03 23:14:54 -0700

    actualizacion productoingrediente dominio

--- RestauranteBDAMain/SistemaRestauranteDominio/src/main/java/itson/sistemarestaurantedominio/ProductoIngrediente.java
+++ RestauranteBDAMain/SistemaRestauranteDominio/src/main/java/itson/sistemarestaurantedominio/ProductoIngrediente.java
@@ -3,10 +3,13 @@ package itson.sistemarestaurantedominio;
 //@author SAUL ISAAC APODACA BALDENEGRO 00000252020
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 
@@ -16,10 +19,47 @@ public class ProductoIngrediente implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idProductoIngrediente")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "idProducto")
+    private Producto producto;
+    
+    @ManyToOne
+    @JoinColumn(name = "idIngrediente")
+    private Ingrediente ingrediente;
+    
+    @Column(name = "cantidad", nullable = false)
+    
+    
     public Long getId() {
         return id;
+    }
+
+    public ProductoIngrediente(Long id, Producto producto, Ingrediente ingrediente) {
+        this.id = id;
+        this.producto = producto;
+        this.ingrediente = ingrediente;
+    }
+
+    public ProductoIngrediente() {
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Ingrediente getIngrediente() {
+        return ingrediente;
+    }
+
+    public void setIngrediente(Ingrediente ingrediente) {
+        this.ingrediente = ingrediente;
     }
 
     public void setId(Long id) {
